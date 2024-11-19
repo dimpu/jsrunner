9fa8b1f581dd21436785e2dd714ecf9cfc182b09
Dimpu Aravind Buddha <dimpu.buddha@bytedance.com>
2024-11-18 17:34:54 -0800

    feat(): Search libs

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a702e535'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j4gb44hc708"
+    "revision": "0.r22umoev288"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
 import svgr from "vite-plugin-svgr";
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     svgr(),
